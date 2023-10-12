399a31798621fbf0a398d68159a224e8fe069abc
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-11 23:19:20 -0600

    creating new project.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Store_main from './components/store_main'
+
+function App() {
+
+  return (
+    <>
+      <Store_main />
+    </>
+  )
+}
+
+export default App
