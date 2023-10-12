9c03c79832927732dd10731bd974cc339823d31c
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-10-11 23:18:36 -0600

    linking product module to main component

---
+++ src/components/Store_main.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import { useState,useEffect } from 'react'
+import Product from './pagina1/products'
+
+const Store_main = () => {
+    return(
+        <>
+            <Product />
+        </>
+    )
+}
+
+export default Store_main
